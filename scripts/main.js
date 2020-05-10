85434d6c1c3e52ff39d67be42f2ff2dda9d0fcd6
Tori Fife <torifife@gmail.com>
2020-05-09 22:44:22 -0600

    add week 3 team activity and notes; populate nav in javascript to make changes easier (there's sometimes a flicker, but I'm okay with it)

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,34 @@ const toggleMenuVisibility = () => {
     document.querySelector('#navigation').classList.toggle('visible')
 }
 
-document.querySelector('#menuButton').addEventListener('click', toggleMenuVisibility)
+function showNav(depth) {
+
+    let pathPrefix = ''
+    for (i = 0; i < depth; i++) {
+        console.log('step')
+        pathPrefix = pathPrefix + '../'
+    }
+    console.log(pathPrefix)
+    console.log(depth)
+    console.log(parseInt(depth))
+
+    document.querySelector('#nav').innerHTML = 
+    "<ul id='navigation'>" +
+            "<li><a href='#menu' id='menuButton'>Menu &equiv;</a></li>" +
+            "<li><a href='" + pathPrefix + "week01/index.html'>Week 01 - Introduction to the Course</a></li>" +
+            "<li><a href='" + pathPrefix + "week02/index.html'>Week 02 - Review of the Basics</a></li>" +
+            "<li><a href='" + pathPrefix + "week03/index.html'>Week 03 - Objects and Arrays</a></li>" +
+            "<!--<li><a href='" + pathPrefix + "week04'>Week 04</a></li>" +
+            "<li><a href='" + pathPrefix + "week05'>Week 05</a></li>" +
+            "<li><a href='" + pathPrefix + "week06'>Week 06</a></li>" +
+            "<li><a href='" + pathPrefix + "week07'>Week 07</a></li>" +
+            "<li><a href='" + pathPrefix + "week08'>Week 08</a></li>" +
+            "<li><a href='" + pathPrefix + "week09'>Week 09</a></li>" +
+            "<li><a href='" + pathPrefix + "week10'>Week 10</a></li>" +
+            "<li><a href='" + pathPrefix + "week11'>Week 11</a></li>" +
+            "<li><a href='" + pathPrefix + "week12'>Week 12</a></li>" +
+            "<li><a href='" + pathPrefix + "week13'>Week 13</a></li> -->" +
+        "</ul>"
+        
+    document.querySelector('#menuButton').addEventListener('click', toggleMenuVisibility)
+}
